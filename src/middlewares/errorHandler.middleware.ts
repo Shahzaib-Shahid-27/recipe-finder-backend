69a57daef5165a2c5b607dc0777ad8969bd5af08
Shahzaib Shahid <shahzaibshahidr10@gami.com>
2026-09-21 22:53:08 +0500

    ap..ts

--- src/middlewares/errorHandler.middleware.ts
+++ src/middlewares/errorHandler.middleware.ts
@@ -1,5 +1,5 @@
 import chalk from 'chalk';
-import AppError from "../utils/AppError.ts";
+import AppError from "../utils/AppError.js";
 import logger from '../utils/logger.ts';
 import { Request, Response, NextFunction } from 'express';
 
