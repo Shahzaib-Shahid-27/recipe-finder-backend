74d2ac0e3a5a472e93f70ff36a68255a1f4e63bc
Shahzaib Shahid <shahzaibshahidr10@gami.com>
2026-09-22 19:17:29 +0500

    fix railway server binding

--- server.ts
+++ server.ts
@@ -40,22 +40,17 @@ const allowedOrigins = [
 
 app.use(
   cors({
-   origin: function (origin, callback) {
-  console.log("CORS Origin:", origin);
+    origin: function (origin, callback) {
+      if (!origin) {
+        return callback(null, true);
+      }
 
-  if (!origin) {
-    return callback(null, true);
-  }
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      }
 
-  if (allowedOrigins.includes(origin)) {
-    return callback(null, true);
-  }
-
-  console.log("CORS BLOCKED:", origin);
-
-  return callback(new Error("Not allowed by CORS"));
-},
-
+      return callback(new Error("Not allowed by CORS"));
+    },
     credentials: true,
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
