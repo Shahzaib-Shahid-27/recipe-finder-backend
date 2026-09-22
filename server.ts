f204b68b888133dabdba0d66b817f1cfbaed65e1
Shahzaib Shahid <shahzaibshahidr10@gami.com>
2026-09-22 18:55:03 +0500

    cors fix

--- server.ts
+++ server.ts
@@ -40,6 +40,15 @@ const allowedOrigins = [
 
   ];
 
+  app.use(
+  cors({
+    origin: process.env.FRONTEND_URL ,
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
 app.use(helmet({
 contentSecurityPolicy: {
     directives: {
@@ -72,20 +81,6 @@ app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.use(
-  cors({
-    origin: (origin, callback) => {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  })
-);
 
 // ========================================
 // ROUTES - MUST BE HERE, AFTER MIDDLEWARE
